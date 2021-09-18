32adcafc9bd1981a73291c4cb50e2ae5ef729dc0
lokified <sheldon.obonyo@student.moringaschool.com>
2021-09-18 21:42:10 +0300

    add a function to calculate the total charges of pizza

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+// Business logic
+function totalOrders(size,crust,topping,orders){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = topping;
+    this.orders = orders;
+}
+
+totalOrders.prototype.totalCharges = function(){
+    return (this.size + this.crust + this.toppings || this.size + this.crust || this.crust + this.toppings || this.size + this.toppings) * this.orders;
+} 
+
+// user interface
 $(document).ready(function(){
     
     // shows information to complete order
