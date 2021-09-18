17f9345e8c6579b1d2bbe65a89b7876cb4663c17
lokified <sheldon.obonyo@student.moringaschool.com>
2021-09-18 22:02:46 +0300

    collects values from the user to display the total charges

--- scripts.js
+++ scripts.js
@@ -16,10 +16,29 @@ $(document).ready(function(){
     // shows information to complete order
     $('.btn').click(function(){
         $('.order').show();
-    })
+    });
     
     // hides the information
     $('#cancel').click(function(){
         $('.order').hide();
-    })
+    });
+
+    // collects values of the order
+    $('#total-charge').last().click(function(){
+        let size = parseInt($('input:radio[name=size]:checked').val());
+        let crust = parseInt($('input:radio[name=crust]:checked').val());
+        let topping =[];
+            $.each($('input:checkbox[name=topping]:checked'),function(){
+                topping.push(parseInt($(this).val()))
+            });
+            let totalToppings = 0;
+            for(let i=0; i<topping.length; i++){
+                totalToppings += topping[i];
+            }
+        let numberOfPizza = parseInt($('#orders-selected').val());
+
+        let totalPizzaCharge = new totalOrders(size,crust,totalToppings,numberOfPizza);
+
+        $('#pizzaCharges').text("Ksh."+totalPizzaCharge.totalCharges());
+    });
 });
